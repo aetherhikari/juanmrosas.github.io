db29cc1fb08f660654ec0e34280e86a8eae6c827
Juan Manuel Rosas <76709765+aetherhikari@users.noreply.github.com>
2023-03-09 02:06:06 +0000

    cum

---
+++ scripts.js
@@ -0,0 +1,56 @@
+function openPage(pageName, elmnt, color) {
+    var i, cardcontent, cardlinks;
+    cardcontent = document.getElementsByClassName("cardcontent");
+    for (i = 0; i < cardcontent.length; i++) {
+        cardcontent[i].style.display = "none";
+    }
+    cardlinks = document.getElementsByClassName("cardlink");
+    for (i = 0; i < cardlinks.length; i++) {
+        cardlinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(pageName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+}
+
+function openJob(evt, jobName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(jobName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen").click();
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
